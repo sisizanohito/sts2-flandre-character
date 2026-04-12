9dd9abef2c11d0b6941bb6aefd1e520a812dc66e
sisizanohito <sisizanohito@gmail.com>
2026-04-13 02:56:32 +0900

    feat: add twisted play card

---
+++ Code/Cards/TwistedPlayCard.cs
@@ -0,0 +1,50 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaseLib.Abstracts;
+using BaseLib.Utils;
+using FlandreMod.Characters;
+using FlandreMod.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace FlandreMod.Cards;
+
+[Pool(typeof(FlandreCharacterCardPool))]
+public sealed class TwistedPlayCard : CustomCardModel
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new PowerVar<MadnessPower>(1m),
+        new IntVar("Draw", 1m)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<MadnessPower>()];
+
+    public override string PortraitPath => "res://images/packed/card_portraits/silent/backflip.png";
+    public override string BetaPortraitPath => PortraitPath;
+
+    public TwistedPlayCard()
+        : base(0, CardType.Skill, CardRarity.Common, TargetType.None)
+    {
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        bool hadMadness = Owner.Creature.HasPower<MadnessPower>();
+
+        await PowerCmd.Apply<MadnessPower>(Owner.Creature, DynamicVars["MadnessPower"].BaseValue, Owner.Creature, this);
+
+        if (!hadMadness)
+            return;
+
+        await CardPileCmd.Draw(choiceContext, (int)DynamicVars["Draw"].BaseValue, Owner);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars["Draw"].UpgradeValueBy(1m);
+    }
+}
