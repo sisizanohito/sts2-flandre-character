ed8babbb78c68938fc268fac7d7684f2a71d77a7
sisizanohito <sisizanohito@gmail.com>
2026-04-17 07:15:34 +0900

    cards: expose destruction eye tooltip on mad gaze

--- Code/Cards/MadGazeCard.cs
+++ Code/Cards/MadGazeCard.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using BaseLib.Abstracts;
 using BaseLib.Utils;
 using FlandreMod.Characters;
+using FlandreMod.Keywords;
 using FlandreMod.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
@@ -15,6 +16,7 @@ namespace FlandreMod.Cards;
 [Pool(typeof(FlandreCharacterCardPool))]
 public sealed class MadGazeCard : CustomCardModel
 {
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [DestructionEye.CustomType];
     protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<MadnessPower>(1m)];
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<MadnessPower>()];
 
